6e749e07f33b4260c124e8ccbf843eea1d30c156
Freebo12 <Tumechko12@gmail.com>
2023-05-30 22:11:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
+
+// const mongoose = require("mongoose");
+// const app = require("./app");
+
+// const DB_HOST =
+//   "mongodb+srv://freebo:stcxTCH8Yh3m3aEu@nodejs.uvdyukn.mongodb.net/Contacts-app?retryWrites=true&w=majority";
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     app.listen(3000, () => {
+//       console.log("Database connection successful");
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
